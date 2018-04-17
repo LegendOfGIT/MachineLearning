3964dc3b47f160e32c6d5fdfc692fb651d7f3231
marcel schneider <marcel.marschner@arvato.com>
2018-04-17 17:08:02 +0200

    - improved training information

--- SimpleFoodPrediction/trainingData.js
+++ SimpleFoodPrediction/trainingData.js
@@ -2,18 +2,25 @@ const food_beer = { beer: 1 };
 const food_burger = { burger: 1 };
 const food_burger_coffee = { burger: 1, coffee: 1 };
 const food_burger_fries = { burger: 1, fries: 1 };
-const food_burger_fries_coke = { burger: 1, fries: 1, coke: 1 };
+const food_burger_fries_softdrink = { burger: 1, fries: 1, softdrink: 1 };
 const food_burger_beer = { burger: 1, beer: 1 };
 const food_burger_fries_beer = { burger: 1, fries: 1, beer: 1 };;
 const food_buns_coffee = { buns: 1, coffee: 1 };
 const food_coffee = { coffee: 1 };
 const food_coffee_cake = { coffee: 1, cake: 1 };
+const food_softdrink_cake = { softdrink: 1, cake: 1 };
+const food_pancakes_coffee = { coffee: 1, pancakes: 1 };
+const food_pancakes_orangejuice = { orangejuice: 1, pancakes: 1 };
+const food_buns_orangejuice =  { orangejuice: 1, buns: 1 };
+const food_bacon_egg_coffee =  { bacon: 1, egg: 1, coffee: 1 };
+const food_sandwich_softdrink =  { softdrink: 1, sandwich: 1 };
+const food_sandwich =  { sandwich: 1 };
 
 const food_propabilities = [
 	{
 		timeOfDayFrom: 0,	//	00:00	
 		timeOfDayTo: 3600,	//	01:00
-		averageNumberOfGuestsPerMinute: 15,
+		averageNumberOfGuestsPerMinute: 3,
 		propabilities: [
 			{ percentage: 75, food: food_coffee },
 			{ percentage: 15, food: food_burger_fries },
@@ -40,6 +47,32 @@ const food_propabilities = [
 			{ percentage: 40, food: food_buns_coffee },
 			{ percentage: 10, food: food_burger },
 			{ percentage: 10, food: food_burger_coffee },
+		]
+	},
+	{
+		timeOfDayFrom: 21601,	//	06:00	
+		timeOfDayTo: 43200,		//	12:00
+		averageNumberOfGuestsPerMinute: 15,
+		propabilities: [
+			{ percentage: 10, food: food_coffee },
+			{ percentage: 50, food: food_buns_coffee },
+			{ percentage: 19, food: food_pancakes_coffee },
+			{ percentage: 5, food: food_pancakes_orangejuice },
+			{ percentage: 8, food: food_buns_orangejuice },
+			{ percentage: 8, food: food_bacon_egg_coffee },			
+		]
+	},
+	{
+		timeOfDayFrom: 43201,	//	12:00	
+		timeOfDayTo: 50400,		//	14:00
+		averageNumberOfGuestsPerMinute: 25,
+		propabilities: [
+			{ percentage: 30, food: food_coffee },
+			{ percentage: 42, food: food_coffee_cake },
+			{ percentage: 3, food: food_softdrink_cake },		
+			{ percentage: 8, food: food_burger_fries_softdrink },			
+			{ percentage: 12, food: food_sandwich_softdrink },
+			{ percentage: 5, food: food_sandwich },			
 		]
 	},
 ];
